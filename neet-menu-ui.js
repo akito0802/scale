8bf94b998f2698647696c48b3f7c9cee3f803200
akito0802 <light.of.king.a@gmail.com>
2026-08-17 20:10:36 +0900

    Load right-top shared menu in scale dictionary

--- neet-menu-ui.js
+++ neet-menu-ui.js
@@ -3,7 +3,7 @@
 const ROOT='https://akito0802.github.io/NEET-note/';
 if(window.__NEET_MENU_V4_LOADER__)return;
 const s=document.createElement('script');
-s.src=ROOT+'global-menu-v4.js?v=20260817-5';
+s.src=ROOT+'global-menu-v4.js?v=20260817-6';
 s.defer=true;
 document.body.appendChild(s);
 })();
